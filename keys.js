62db0092729f613731e2b6a058f75e66214a8041
admin <admin@admins-MacBook-Pro.local>
2019-10-31 13:00:42 -0700

    added spotify api

--- keys.js
+++ keys.js
@@ -2,5 +2,6 @@ console.log("this is loeade");
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
-  };
-  
+}  
+
+
--- liri.js
+++ liri.js
@@ -1,13 +1,35 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var fs = require("fs");
+var Spotify = require('node-spotify-api');
+fs.readFile("random.txt", "utf8", function (error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+        return console.log(error);
+    }
+
+    // We will then print the contents of data
+    console.log(data);
+
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+
+    // We will then re-display the content as an array for later use.
+    console.log(dataArr);
+
+});
+// spotify-this-song// 
+var artist=" "
+var songName = " "
+var axios = require("axios");
 
 var spotify = new Spotify(keys.spotify);
 
-//Make it so liri.js can take in one of the following commands:
-//concert-this
-//spotify-this-song
-//movie-this
-//do-what-it-says///
+var queryURL = "https://api.spotify.com/v1/search?q=track:" + songName + "%20artist:" + artist + "&type=track&limit=10";
+spotify.request(queryURL).then(function (data) {
+    console.log(data);
+})
 
 
-
+//
